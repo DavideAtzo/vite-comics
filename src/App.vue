a544ed0fd1627e6b3a54c43ba752af10c3197a80
DavideAtzo <atzo9477@gmail.com>
2023-03-24 16:05:26 +0100

    aggiunta logo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeaderLogo from "./components/AppHeaderLogo.vue";
+  export default {
+    components: {
+      AppHeaderLogo
+    }
+  }
+</script>
+
+<template>
+  <AppHeaderLogo></AppHeaderLogo>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHeaderLogo.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+<template>
+    <header>
+        <div class="container">
+            <div id="logoHeader">
+            <img src="../assets/img/dc-logo.png" alt="logo-DC">
+        </div>
+        </div>
+    </header>
+</template>
+<style></style>
