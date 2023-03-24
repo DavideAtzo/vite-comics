c728d0064a3c9b824ea92b6f345eceb35b132992
DavideAtzo <atzo9477@gmail.com>
2023-03-24 16:12:20 +0100

    aggiunta dei link header

--- src/components/AppHeaderLogo.vue
+++ src/components/AppHeaderLogo.vue
@@ -2,7 +2,48 @@
 export default {
     data() {
         return {
-            
+            navLinks: [
+                {
+                    tag: "characters",
+                    link: "#"
+                },
+                {
+                    tag: "comics",
+                    link: "#"
+                },
+                {
+                    tag: "movies",
+                    link: "#"
+                },
+                {
+                    tag: "tv",
+                    link: "#"
+                },
+                {
+                    tag: "games",
+                    link: "#"
+                },
+                {
+                    tag: "collectibles",
+                    link: "#"
+                },
+                {
+                    tag: "videos",
+                    link: "#"
+                },
+                {
+                    tag: "fans",
+                    link: "#"
+                },
+                {
+                    tag: "news",
+                    link: "#"
+                },
+                {
+                    tag: "shop",
+                    link: "#"
+                }
+            ]
         }
     }
 }
@@ -11,9 +52,14 @@ export default {
     <header>
         <div class="container">
             <div id="logoHeader">
-            <img src="../assets/img/dc-logo.png" alt="logo-DC">
-        </div>
+                <img src="../assets/img/dc-logo.png" alt="logo-DC">
+            </div>
+            <div id="navLinks">
+                <ul>
+                    <li v-for="link in navLinks"><a href="link.link">{{ link.tag }}</a></li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
-<style></style>
+<style lang="scss" scoped></style>
