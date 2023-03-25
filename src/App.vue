871fa4da89a65bbf338f58cd387153248cde7f5b
DavideAtzo <atzo9477@gmail.com>
2023-03-25 12:28:29 +0100

    sistemazione file per il footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppMainContent from "./components/AppMainContent.vue";
-import AppTopFooter from "./components/AppTopFooter.vue";
-import AppMainFooter from "./components/AppMainFooter.vue";
+// import AppTopFooter from "./components/AppTopFooter.vue";
+// import AppMainFooter from "./components/AppMainFooter.vue";
+import AppFooter from './components/AppFooter.vue';
   export default {
     components: {
       AppHeader,
       AppMainContent,
-      AppTopFooter,
-      AppMainFooter
+      // AppTopFooter,
+      // AppMainFooter
+      AppFooter,
     }
   }
 </script>
@@ -16,8 +18,9 @@ import AppMainFooter from "./components/AppMainFooter.vue";
 <template>
   <AppHeader></AppHeader>
   <AppMainContent></AppMainContent>
-  <AppTopFooter></AppTopFooter>
-  <AppMainFooter></AppMainFooter>
+  <AppFooter></AppFooter>
+  <!-- <AppTopFooter></AppTopFooter>
+  <AppMainFooter></AppMainFooter> -->
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTopFooter from './AppTopFooter.vue';
+import AppMainFooter from './AppMainFooter.vue';
+export default {
+    components: {
+        AppTopFooter,
+        AppMainFooter,
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <footer>
+        <AppTopFooter></AppTopFooter>
+        <AppMainFooter></AppMainFooter>
+    </footer>
+</template>
+
+<style lang="scss" scoped></style>
