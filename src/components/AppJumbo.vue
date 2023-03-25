c1d8886ea74c4e7cffe057f0083b1e3ea9fe54a9
DavideAtzo <atzo9477@gmail.com>
2023-03-25 21:27:36 +0100

    aggiunto jumbo

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    data() {
+      
+    }
+  }
+</script>
+
+<template>
+  <section class="jumbo">
+
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.jumbo{
+  height: 25rem;
+  background-image: url('../assets/img/jumbotron.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -1,26 +1,18 @@
 <script>
+import AppJumbo from "./AppJumbo.vue";
+export default {
+    components: {
+        AppJumbo
+    }
+  }
 </script>
 
 <template>
     <main>
-        <div class="container">
-            <h2>...Content go here ...</h2>
-        </div>
+        <AppJumbo></AppJumbo>
     </main>
 </template>
 
 <style lang="scss" scoped>
-main {
-    background-color: black;
-    .container {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 50px 0;
-    
 
-    h2 {
-        color: white;
-    }
-}
-}
 </style>
